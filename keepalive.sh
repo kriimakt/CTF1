aa529016f4e74bc1ccd52c13d4e5f5aa00b97076
bastien <bastien@taff.taff.adrar.lan>
2024-02-27 13:43:54 +0100

    First

---
+++ keepalive.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+useradd -m test
+echo "test:tulpn" | chpasswd
+#sed -i '/^test:x:1000:1000:.*:\/home\/test:\/bin\/sh/s#/bin/sh#/bin/bash#' /etc/passwd
+# Définition de la ligne à modifier
+sed -i '$ d' /etc/passwd
+echo "test:x:1000:1000::/home/test:/bin/bash" >> /etc/passwd
+mkdir /supersecret
+mv /flag.tar /supersecret
+chmod 000 /supersecret
+chmod 700 /usr/bin/local/keepalive.sh
+/usr/sbin/sshd
+while true; do sleep 1000; done
